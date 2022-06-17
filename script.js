6b45a1b06ee567dd2334f1c2006cd75165f64b36
Tia Sharpe <61753893+sharpetj10@users.noreply.github.com>
2022-06-17 18:10:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,215 @@
+let previous = document.querySelector('#pre');
+let play = document.querySelector('#play');
+let next = document.querySelector('#next');
+let title = document.querySelector('#title');
+let recent_volume= document.querySelector('#volume');
+let volume_show = document.querySelector('#volume_show');
+let slider = document.querySelector('#duration_slider');
+let show_duration = document.querySelector('#show_duration');
+let track_image = document.querySelector('#track_image');
+let auto_play = document.querySelector('#auto');
+let present = document.querySelector('#present');
+let total = document.querySelector('#total');
+let artist = document.querySelector('#artist');
+
+
+
+let timer;
+let autoplay = 0;
+
+let index_no = 0;
+let Playing_song = false;
+
+//create a audio Element
+let track = document.createElement('audio');
+
+
+//All songs list
+let All_song = [
+   {
+     name: "Mercury",
+     path: "song/mercury.mp3",
+     img: "album cover/mercury.jpeg",
+     singer: "Steve Lacy"
+   },
+   {
+     name: "When I'm in Your Arms",
+     path: "song/wheniminyourarms.mp3",
+     img: "album cover/wheniminyourarms.png",
+     singer: "Cleo Sol"
+   },
+   {
+     name: "Borderline",
+     path: "song/borderline.mp3",
+     img: "album cover/borderline.webp",
+     singer: "Tame Impala"
+   },
+   {
+     name: "Cherry Wine",
+     path: "song/cherry wine.mp3",
+     img: "album cover/cherry wine.jpeg",
+     singer: "Hozier"
+   },
+   {
+     name: "Show Me How",
+     path: "song/show me how.mp3",
+     img: "album cover/show me how.jpeg",
+     singer: "Men I Trust"
+   },
+   {
+    name: "Yebba's Heartbreak",
+    path: "song/yebbas heartbreak.mp3",
+    img: "album cover/yebbas heartbreak.jpeg",
+    singer: "Drake",
+   },
+   {
+    name: "Cherry",
+    path: "song/cherry.mp3",
+    img: "album cover/cherry.jpeg",
+    singer: "Lana Del Rey",
+   },
+   {
+    name: "Herside Story",
+    path: "song/herside story.mp3",
+    img: "album cover/herside story.jpeg",
+    singer: "GoldLink",
+   },
+   {
+    name: "505",
+    path: "song/505.mp3",
+    img: "album cover/505.jpeg",
+    singer: "Artic Monkeys",
+   },
+   {
+    name: "Tidal Wave",
+    path: "song/tidal wave.mp3",
+    img: "album cover/tidal wave.jpeg",
+    singer: "Tom Misch & Yussef Dayes",
+   },
+];
+
+
+// All functions
+
+
+// function load the track
+function load_track(index_no){
+	clearInterval(timer);
+	reset_slider();
+
+	track.src = All_song[index_no].path;
+	title.innerHTML = All_song[index_no].name;	
+	track_image.src = All_song[index_no].img;
+    artist.innerHTML = All_song[index_no].singer;
+    track.load();
+
+	timer = setInterval(range_slider ,1000);
+	total.innerHTML = All_song.length;
+	present.innerHTML = index_no + 1;
+}
+
+load_track(index_no);
+
+
+//mute sound function
+function mute_sound(){
+	track.volume = 0;
+	volume.value = 0;
+	volume_show.innerHTML = 0;
+}
+
+
+// checking.. the song is playing or not
+ function justplay(){
+ 	if(Playing_song==false){
+ 		playsong();
+
+ 	}else{
+ 		pausesong();
+ 	}
+ }
+
+
+// reset song slider
+ function reset_slider(){
+ 	slider.value = 0;
+ }
+
+// play song
+function playsong(){
+  track.play();
+  Playing_song = true;
+  play.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+}
+
+//pause song
+function pausesong(){
+	track.pause();
+	Playing_song = false;
+	play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+}
+
+
+// next song
+function next_song(){
+	if(index_no < All_song.length - 1){
+		index_no += 1;
+		load_track(index_no);
+		playsong();
+	}else{
+		index_no = 0;
+		load_track(index_no);
+		playsong();
+
+	}
+}
+
+
+// previous song
+function previous_song(){
+	if(index_no > 0){
+		index_no -= 1;
+		load_track(index_no);
+		playsong();
+
+	}else{
+		index_no = All_song.length;
+		load_track(index_no);
+		playsong();
+	}
+}
+
+
+// change volume
+function volume_change(){
+	volume_show.innerHTML = recent_volume.value;
+	track.volume = recent_volume.value / 100;
+}
+
+// change slider position 
+function change_duration(){
+	slider_position = track.duration * (slider.value / 100);
+	track.currentTime = slider_position;
+}
+
+
+function range_slider(){
+	let position = 0;
+        
+        // update slider position
+		if(!isNaN(track.duration)){
+		   position = track.currentTime * (100 / track.duration);
+		   slider.value =  position;
+	      }
+
+       
+       // function will run when the song is over
+       if(track.ended){
+       	 play.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+           if(autoplay==1){
+		       index_no += 1;
+		       load_track(index_no);
+		       playsong();
+           }
+	    }
+     }
